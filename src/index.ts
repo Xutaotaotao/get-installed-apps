2ad0b28f9492d5e5bdec875ae733723834c98b8d
Jarvie <jarvie@JarviedeMacBook-Air.local>
2023-07-14 17:37:32 +0800

    change

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,10 @@
-import {getInstalledApps as macGetInstalledApps} from './mac'
-import {getInstalledApps as winGetInstalledApps} from './win'
+import { getInstalledApps as macGetInstalledApps } from './mac'
+import { getInstalledApps as winGetInstalledApps } from './win'
 
-export function getInstalledApps() {
-  if (process.platform === 'darwin') {
-    return macGetInstalledApps()
-  } else if (process.platform === 'win32') {
-    return winGetInstalledApps()
-  } else {
-    return new Promise((_resolve,reject) => {
-      reject('Platform not supported')
-    })
-  }
+export function getMacInstalledApps(directory = "/Applications") {
+  return macGetInstalledApps(directory)
 }
 
-export function getMacInstalledApps () {
-  return macGetInstalledApps()
-}
-
-export function getWinInstalledApps () {
+export function getWinInstalledApps() {
   return winGetInstalledApps()
 }
