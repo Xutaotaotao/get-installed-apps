606789b22acf1a16fd551b0a5bd322345212e220
Xutaotaotao <xutaotao1108@outlook.com>
2024-07-24 20:16:19 +0800

    test: getAppLocation

--- example.js
+++ example.js
@@ -1,5 +1,61 @@
 const { getInstalledApps } = require("./dist/index");
+const fs = require("fs");
+const path = require("path");
 
-getInstalledApps().then(apps => {
-    console.log(apps)
-})
+function getDirectoryFromPath(filePath) {
+  return path.dirname(filePath);
+}
+
+function listExeFiles(directoryPath) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(directoryPath, (err, files) => {
+      if (err) {
+        reject(`Error reading directory ${directoryPath}: ${err}`);
+        return;
+      }
+
+      const exeFiles = files
+        .filter(
+          (file) =>
+            path.extname(file).toLowerCase() === ".exe" &&
+            !file.toLowerCase().includes("uni")
+        )
+        .map((file) => path.join(directoryPath, file));
+
+      resolve(exeFiles);
+    });
+  });
+}
+
+const getAppLocation = async (app) => {
+  let location = [];
+  if (app.DisplayIcon && app.DisplayIcon.includes(".exe")) {
+    location = app.DisplayIcon.split(",")[0];
+  } else {
+    if (app.UninstallString && app.UninstallString.includes(".exe")) {
+      const uninstalllocation = app.UninstallString.split('"')[1];
+      const directoryPath = uninstalllocation
+        ? getDirectoryFromPath(uninstalllocation)
+        : "";
+      if (directoryPath) {
+        const exeFiles = await listExeFiles(directoryPath);
+        location = exeFiles;
+      }
+    }
+  }
+  return location;
+};
+
+getInstalledApps().then(async (apps) => {
+  Promise.all(
+    apps.filter(o => o.appPublisher && !o.appPublisher.includes('Microsoft')).map(async (o) => {
+      const appLocation = await getAppLocation(o);
+      return {
+        ...o,
+        appLocation,
+      };
+    })
+  ).then((res) => {
+    console.log(res);
+  });
+});
